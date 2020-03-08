1cac564c25b4c80fba9e673d6c202017ac4cb5ca
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 19:09:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -83,7 +83,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  consolidate_cart(cart)
   puts cart
   consolidated_cart = consolidate_cart(cart)
   puts consolidated_cart
