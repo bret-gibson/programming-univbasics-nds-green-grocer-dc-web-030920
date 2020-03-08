db682efbe4ea5af71debd889aad877c8cbcdea31
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 18:02:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,6 @@
 require "pry"
 
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
   output = nil
   collection.each do |key|
     if name == key[:item]
