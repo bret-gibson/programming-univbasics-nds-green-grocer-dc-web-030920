c3d86f4663d80cf6c82af077939d6d90f1cd8489
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 18:01:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,6 +8,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.each do |key|
     if name == key[:item]
       output = key
+      return output
     else
       output = nil
     end
