0ea2943d7b4c0a17b984b349c285756a4577ee79
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 19:07:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -86,14 +86,15 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
-  =begin
-  grand_total = 0
-  final_cart.each do |key|
-    grand_total += (key[:price] * key[:count])
-  end
-  if grand_total > 100
-    total -= (total * 0.1)
-  end
-  puts grand_total
-  =end
 end
+
+=begin
+grand_total = 0
+final_cart.each do |key|
+  grand_total += (key[:price] * key[:count])
+end
+if grand_total > 100
+  total -= (total * 0.1)
+end
+puts grand_total
+=end
