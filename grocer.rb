0ddaf60cd2f00496ed43c877e47ae096d03494cb
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 18:21:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  coupons.each do |key, value|
+  coupons.each do |key|
     binding.pry
   end
 end
